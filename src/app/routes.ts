9d1073f2585ee285b05673357a063a7f9954de6a
Paranorman2049 <johnoluwaseungabriel@gmail.com>
2022-12-15 23:57:09 +0100

    Added the how-to page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,10 @@
-import  {Routes } from '@angular/router'
+import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './pages/about.component';
 import { ServicesComponent } from './pages/services.component';
 import { HireUsComponent } from './pages/hireus.component';
 import { ContactUsComponent } from './pages/contactus.component';
+import { HowToComponent } from './pages/howto.component';
 
 
 export const appRoutes:Routes = [
@@ -12,5 +13,7 @@ export const appRoutes:Routes = [
     { path: 'services', component: ServicesComponent },
     { path: 'hire', component: HireUsComponent },
     { path: 'contact', component: ContactUsComponent },
+    { path: 'howto', component: HowToComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
+    //configure a 404 page
 ]
